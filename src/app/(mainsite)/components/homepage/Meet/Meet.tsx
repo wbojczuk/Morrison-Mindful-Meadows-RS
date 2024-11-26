271951d9104dd657fbbd6228931764189b21b034
William Bojczuk <williambojczuk@gmail.com>
2024-11-26 16:15:32 -0500

    updated

--- src/app/(mainsite)/components/homepage/Meet/Meet.tsx
+++ src/app/(mainsite)/components/homepage/Meet/Meet.tsx
@@ -9,7 +9,7 @@ export default function Meet() {
   return (
     <section className={styles.aboutUs}>
         <div className={styles.textWrapper}>
-          <h4>Meet Marcia</h4>
+          <h4>About Me</h4>
             <h1>Meet Dr. Marcia <span className="underline">Morrison</span></h1>
 
             <p>My goal is to help individuals and couples reach their personal and relationship goals, creating lasting positive change. Outside of my practice, I’m dedicated to continuing my education and staying up-to-date with the latest research in counseling. I work hard to provide a warm, welcoming, and non-judgmental space where you feel heard, valued, and supported. I’m excited to be a part of your journey toward better emotional well-being and more fulfilling relationships.</p>
